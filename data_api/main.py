f9e1775a39c060e2a0c04c11a14e49961b4c7cb2
garzonds <garzonds@javeriana.edu.co>
2026-03-17 00:15:46 -0500

    Increase minimum update time to 300 seconds

--- data_api/main.py
+++ data_api/main.py
@@ -7,7 +7,7 @@ import time
 import csv
 import os
 
-MIN_UPDATE_TIME = 60 ## Aca pueden cambiar el tiempo minimo para cambiar bloque de información
+MIN_UPDATE_TIME = 300 ## Aca pueden cambiar el tiempo minimo para cambiar bloque de información
 
 # ---------------------------
 # Metadatos y documentación
